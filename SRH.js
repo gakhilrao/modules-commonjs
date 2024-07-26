dacfa841b94cf015e086612fdf9cb35c95929da9
gakhilrao <gullaakhilrao@gmail.com>
2024-07-26 13:44:56 +0530

    Add files via upload

---
+++ SRH.js
@@ -0,0 +1,9 @@
+let franchiseName ="Sunrisers Hyderabad";
+let players = ["Pat Cummins","Travis Head","Abhishek Sharma","Nithish Reddy","Bhuvaneshwar Kumar"];
+
+let acheivementsOfSRH = ()=>{
+    console.log("The team made their first IPL appearance in 2013, where they reached the playoffs, eventually finishing in fourth place. The Sunrisers won their maiden IPL title in the 2016 season, defeating Royal Challengers Bangalore by 8 runs in the final.");
+
+}
+
+module.exports = {franchiseName,players,acheivementsOfSRH};
---
+++ server.js
@@ -0,0 +1,5 @@
+let {franchiseName,players,acheivementsOfSRH} = require("./SRH");
+
+console.log(franchiseName);
+console.log(players);
+acheivementsOfSRH();
